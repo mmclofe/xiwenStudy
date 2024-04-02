65ccd6ddddc946702e38b0b65add37dc7bacfdc4
希文 <1164784711@qq.com>
2024-04-02 13:53:47 +0800

    fix: 1

--- javaScript/proxy/proxy(get、set).js
+++ javaScript/proxy/proxy(get、set).js
@@ -1,17 +1,17 @@
 const handler = {
   get: function (obj, prop, receiver) {
-    console.log("读取",receiver)
+    console.log("读取",obj,prop,receiver)
     return prop in obj ? obj[prop] : "没有该属性";
   },
   set: function (obj, prop, value, receiver) {
     if(obj[prop] !== value){
-      console.log("改变")
+      console.log("改变",obj, prop)
       obj[prop] = value
     }
     return true // 设置成功
   }
 };
-
-const p = new Proxy({}, handler);
-p.a = 1;
-console.log(p.a)
+const a = { b:"1",c:{d:"2",e:"3"}}
+const p = new Proxy(a, handler);
+// proxy不能深层代理
+console.log(p.c.d)
