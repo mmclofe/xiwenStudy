7b159afb0888d517886174afefeaeac8073b9dd7
唐占东 <tangzhandong@harmonycloud.cn>
2025-06-16 14:14:32 +0800

    fix: 1

--- javaScript/算法/LRU缓存-美团.js
+++ javaScript/算法/LRU缓存-美团.js
@@ -1,7 +1,7 @@
 // LRU（Least Recently Used，最久未使用）算法是一种常用的缓存淘汰策略，
 // 当缓存达到其容量上限时，它会移除那些最久没有被访问的数据项。这种策略基于这样一个假设：
 // 如果一个数据项在近期没有被访问过，那么在未来一段时间内也不太可能被访问。
-// 实现LRUCache类，并提供put方法和get方法。put方法zai写入缓存，写入缓存之前需判断缓存是否已满，如果已满，需要删除最久未使用的数据项，再写入数据。get方法读取缓存，未读取到缓存，返回-1。
+// 实现LRUCache类，并提供put方法和get方法。put方法写入缓存，写入缓存之前需判断缓存是否已满，如果已满，需要删除最久未使用的数据项，再写入数据。get方法读取缓存，未读取到缓存，返回-1。
 class LRUCache {
   #capacity
   #cache
@@ -15,7 +15,7 @@ class LRUCache {
   get(key) {
     if (this.has(key)) {// 有该key
       // 获取值，并将该 key 移动到 Map 的末尾表示最近使用过
-      const value = this.cache.get(key);
+      const value = this.#cache.get(key);
       this.#cache.delete(key);
       this.#cache.set(key, value);
       return value;
