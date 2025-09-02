5b789fe4c1348d3f286d6691406bfb776cff1216
唐占东 <tangzhandong@harmonycloud.cn>
2025-09-02 19:52:30 +0800

    fix: 1

--- javaScript/防抖-节流/throttle.js
+++ javaScript/防抖-节流/throttle.js
@@ -2,7 +2,7 @@ function throttle(fn,wait=300){
   let timer = null
   return function(...args) {
     if(timer) {return}
-    setTimeout(() => {
+    timer = setTimeout(() => {
       fn.apply(this,args)
       timer = null
     },wait)
